3f4587638bd529e64490b0e2f9ae5bebd4c3b606
JPen3 <jerry_pendleton@yahoo.com>
2014-07-04 01:24:33 -0500

    Door Opening Mechanics for White Room
    Door Opening Mechanics for the White Room are set up but I am running
    into an issue with the black wall/door in the Red Room that hasn't be
    changed into a door yet. As it stands right now, it is impossible to
    even enter the red room because of said door/wall. Whereas the door
    opening for the other doors is just a modification of the code that is
    needed.

---
+++ summerColorGame/Assets/scripts/White Room/blueControl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blueControl : MonoBehaviour {
+	string eventName1 = "opendoor";
+	string eventName2 = "closedoor";
+	
+	private magentaRRExit mEObject;
+	
+	public bool initialLock = true;
+	public bool isLocked = true;
+	public bool isOpen = false;
+	public bool isLoaded = false;
+	
+	// Use this for initialization
+	void Start () {
+		//GameObject doorMagenta = GameObject.FindGameObjectWithTag ("doorRRMagenta");
+		//mEObject = doorMagenta.GetComponent<magentaRRExit> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(mEObject.aniTexDone && isLocked)
+		{
+			Debug.Log("Red Room's Magenta Door opens");
+			if(!isLoaded)
+			{
+				Application.LoadLevelAdditiveAsync("blueDaDaDee");
+			}
+			iTweenEvent.GetEvent (GameObject.Find ("hingeRRMagenta"), eventName1).Play ();
+			isLocked = false;
+			isOpen = true;
+			isLoaded = true;
+		}*/
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log ("You in the Blue Room Cyan Trigger");
+		/*if(!initialLock)
+		{
+			if(other.collider.gameObject.CompareTag ("Player"))
+			{
+				//Debug.Log("Magenta Door opens");
+				iTweenEvent.GetEvent (GameObject.Find ("hingeRRMagenta"), eventName2).Play ();
+				initialLock = true;
+			}
+		}*/
+		if(!isOpen)
+		{
+			Debug.Log("Red Door opens");
+			if(!isLoaded)
+			{
+				Application.LoadLevelAdditiveAsync("blueDaDaDee");
+			}
+			iTweenEvent.GetEvent (GameObject.Find ("hingeBlue"), eventName1).Play ();
+			isOpen = true;
+			isLoaded = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		
+		if (isOpen)
+		{
+			if(other.collider.gameObject.CompareTag ("Player"))
+			{
+				//Debug.Log("Magenta Door closes");
+				iTweenEvent.GetEvent (GameObject.Find ("hingeBlue"), eventName2).Play ();
+				isOpen = false;
+			}
+		}
+	}
+	public void setClosed()
+	{
+		isOpen = !isOpen;
+	}
+	
+	public bool getClosed()
+	{
+		return isOpen;
+	}
+}
